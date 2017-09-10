714253b753c10b0f49d04fb193042866910d6ec0
Vitaly Sharapov <akaredsc2@gmail.com>
2017-09-10 20:03:16 +0300

    Add collision detection

--- Assets/PhysicsObject.cs
+++ Assets/PhysicsObject.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PhysicsObject : MonoBehaviour
 {
+    public float minGroundNormalY = 0.65f;
     public float gravityModifier = 1f;
+
+    protected bool isGrounded;
+    protected Vector2 groundNormal;
 
     protected Rigidbody2D rigidbody2D;
     protected Vector2 velocity;
     protected ContactFilter2D contactFilter2D;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
 
-    protected const float minMovementDistance = 0.01f;
+    protected const float minMovementDistance = 0.001f;
     protected const float shellRadius = 0.01f;
 
     private void OnEnable()
@@ -27,18 +33,49 @@ public class PhysicsObject : MonoBehaviour
     private void FixedUpdate()
     {
         velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        isGrounded = false;
         Vector2 deltaPosition = velocity * Time.deltaTime;
         Vector2 movement = Vector2.up * deltaPosition.y;
-        ApplyMovement(movement);
+        ApplyMovement(movement, true);
     }
 
-    private void ApplyMovement(Vector2 movement)
+    private void ApplyMovement(Vector2 movement, bool isVerticalMovement)
     {
         float distance = movement.magnitude;
         if (distance > minMovementDistance)
         {
-            rigidbody2D.Cast(movement, contactFilter2D, hitBuffer, distance + shellRadius);
+            int count = rigidbody2D.Cast(movement, contactFilter2D, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++) 
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                Debug.Log("Current normal " + currentNormal);
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    isGrounded = true;
+                    Debug.Log("Is grounded");
+                    if (isVerticalMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
         }
-        rigidbody2D.position += movement;
+        rigidbody2D.position = rigidbody2D.position + movement.normalized * distance;
     }
 }
