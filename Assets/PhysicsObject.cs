142b184efe06a58dc2f076a898f174f7b848c708
Vitaly Sharapov <akaredsc2@gmail.com>
2017-09-10 22:07:41 +0300

    Refactor

--- Assets/PhysicsObject.cs
+++ Assets/PhysicsObject.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhysicsObject : MonoBehaviour
 {
@@ -34,13 +36,17 @@ public class PhysicsObject : MonoBehaviour
 
     private void Update()
     {
-        targetVelocity = Vector2.zero;
-        ComputeVelocity();
+        targetVelocity = ComputeVelocity();
+        UpdateVisuals();
     }
 
-    protected virtual void ComputeVelocity()
+    protected virtual Vector2 ComputeVelocity()
     {
-        
+        return new Vector2();
+    }
+
+    protected virtual void UpdateVisuals()
+    {
     }
 
     private void FixedUpdate()
@@ -49,11 +55,11 @@ public class PhysicsObject : MonoBehaviour
         velocity.x = targetVelocity.x;
         isGrounded = false;
         Vector2 deltaPosition = velocity * Time.deltaTime;
-        
+
         Vector2 surfaceMovementDirection = new Vector2(groundNormal.y, -groundNormal.x);
         Vector2 surfaceMovement = surfaceMovementDirection * deltaPosition.x;
         ApplyMovement(surfaceMovement, false);
-        
+
         Vector2 verticalMovement = Vector2.up * deltaPosition.y;
         ApplyMovement(verticalMovement, true);
     }
@@ -73,11 +79,9 @@ public class PhysicsObject : MonoBehaviour
             foreach (RaycastHit2D hit2D in hitBufferList)
             {
                 Vector2 currentNormal = hit2D.normal;
-                Debug.Log("Current normal " + currentNormal);
                 if (currentNormal.y > minGroundNormalY)
                 {
                     isGrounded = true;
-                    Debug.Log("Is grounded");
                     if (isVerticalMovement)
                     {
                         groundNormal = currentNormal;
@@ -88,13 +92,13 @@ public class PhysicsObject : MonoBehaviour
                 float projection = Vector2.Dot(velocity, currentNormal);
                 if (projection < 0)
                 {
-                    velocity = velocity - projection * currentNormal;
+                    velocity -= projection * currentNormal;
                 }
 
                 float modifiedDistance = hit2D.distance - shellRadius;
                 distance = Math.Min(modifiedDistance, distance);
             }
         }
-        rigidbody2D.position = rigidbody2D.position + movement.normalized * distance;
+        rigidbody2D.position += movement.normalized * distance;
     }
 }
--- Assets/PlayerControllerComponent.cs
+++ Assets/PlayerControllerComponent.cs
@@ -3,22 +3,21 @@ using UnityEngine;
 
 public class PlayerControllerComponent : PhysicsObject
 {
-    public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
+    public float maxSpeed = 3;
+    public float jumpTakeOffSpeed = 4;
 
     private SpriteRenderer spriteRenderer;
     private Animator animator;
-    
+
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
 
-    protected override void ComputeVelocity()
+    protected override Vector2 ComputeVelocity()
     {
         Vector2 movement = Vector2.zero;
-
         movement.x = Input.GetAxis("Horizontal");
 
         if (Input.GetButtonDown("Jump") && isGrounded)
@@ -33,15 +32,18 @@ public class PlayerControllerComponent : PhysicsObject
             }
         }
 
-        bool isFlipRequired = spriteRenderer.flipX ? movement.x > 0.01f : movement.x < 0.01f;
+        return movement * maxSpeed;
+    }
+
+    protected override void UpdateVisuals()
+    {
+        bool isFlipRequired = spriteRenderer.flipX ? targetVelocity.x > 0.01f : targetVelocity.x < 0.01f;
         if (isFlipRequired)
         {
             spriteRenderer.flipX = !spriteRenderer.flipX;
         }
-        
+
         animator.SetBool("grounded", isGrounded);
         animator.SetFloat("velocityX", Math.Abs(velocity.x) / maxSpeed);
-        
-        targetVelocity = movement * maxSpeed;
     }
 }
