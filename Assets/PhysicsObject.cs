cc88d88391cb14bf7220cb8ae4729dd58fb0802a
Vitaly Sharapov <akaredsc2@gmail.com>
2017-09-10 18:29:49 +0300

    Add physics object with basic gravity

---
+++ Assets/PhysicsObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+
+    protected Rigidbody2D rigidbody2D;
+    protected Vector2 velocity;
+
+    private void OnEnable()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 movement = Vector2.up * deltaPosition.y;
+        ApplyMovement(movement);
+    }
+
+    private void ApplyMovement(Vector2 movement)
+    {
+        rigidbody2D.position += movement;
+    }
+}
