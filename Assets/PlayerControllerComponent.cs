3b4c6291b466109c7cdcb426a16d29790af467d2
Vitaly Sharapov <akaredsc2@gmail.com>
2017-09-10 21:28:43 +0300

    Add player controller

---
+++ Assets/PlayerControllerComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerControllerComponent : PhysicsObject
+{
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 movement = Vector2.zero;
+
+        movement.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y *= 0.5f;
+            }
+        }
+
+        targetVelocity = movement * maxSpeed;
+    }
+}
